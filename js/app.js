7bbaaa1d3fc98638054ce19946f140a6cbf44c6c
amirb <amir.bullock9@gmail.com>
2023-04-03 14:40:14 -0400

    add songs array to iterate through each round

--- js/app.js
+++ js/app.js
@@ -10,25 +10,27 @@ import {
 const numRounds = 4
 const winningScore = 3
 /*---------------------------- Variables (state) ----------------------------*/
-
+// use of file 
+const songs = [
+  {title: "Snow", file: snow},
+  {title: "Warfield", file: warField},
+  {title: "Honey and Bleach", file: hAndB},
+  {title: "Stress Filled", file: stressFilled}
+]
 let round = 1
-let winner = false
-let songEndTime = 0
-let songTitle = ""
+
 let player1Score = 0
 let player2Score = 0
-let globalTimer
-let inputAllowed = false
-let timeInterval
-let player1Image = ""
-let player2Image = ""
+
+let timerIntervalId
 /*------------------------ Cached Element References ------------------------*/
 
-const roundStartTimerEl = document.getElementById('roundStartTimer')
+const startRoundTimerEl = document.getElementById('timer')
 let header = document.querySelector('h1')
 let button = document.querySelector('button')
 const startScreen = document.getElementById("start-screen")
 const startButton = document.getElementById("startButton")
+const roundStartButton = document.getElementById("roundStartButton")
 const gameBoard = document.getElementById("gameboard")
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -46,7 +48,27 @@ resetButton.addEventListener("click", function(){
 function startGame () {
   startScreen.style.display = "none"
   playRandom()
+  startRound()
+}
 
+function startRound() {
+  changeSong()
+  playRandom()
+  endGame()
+}
+
+function startTimer() {
+  // check for active timer intv
+  if (timerIntervalId) {
+    seconds = 0
+    // if interval is active
+    clearInterval(timerIntervalId)
+    renderMessage()
+  }
+}
+
+function renderMessage(){
+  
 }
 
 function makeAnimationGo() {
@@ -66,15 +88,17 @@ function endGame(){
     nextRound()
   }
 }
+
 console.log(makeRandomNum(0, 100))
 function playRandom() {
   //declare if a round is started, if not ignore the timer
+  changeSong()
   if (round < 1){
       // declare random num 
       let randomNumber = Math.floor(Math.random() * 1000) + 5000
       //import sound from seperate js file
-      // play song
-      snow.play()
+      // play song based on what round it is
+      round.song.play()
       // record start time of song playing
       // let songStartTime = Date.now()
       // declare end time 
@@ -82,13 +106,14 @@ function playRandom() {
       console.time('startRoundTimer')
       // set the timeout function to pause based on random delay
       setTimeout(function() {
-      snow.pause()
+      round.song.pause()
       endTime = Date.now()
       let attackButton = document.getElementById('attack')
       attackButton.disabled = false
       attackButton.addEventListener('click', function() {
       let userTime = Date.now()
       let difference = Math.abs(endTime - userTime)
+      //this will print a longer number remember to cut off when displaying
       console.log(`Player input time: ${userTime}`)
       console.log(`Song end time: ${endTime}`)
       console.log(`Difference: ${difference}`)
@@ -99,44 +124,24 @@ function playRandom() {
     //add the randomNumber delay function at end
     }, randomNumber) //generate rand time btw 5-15 secs
     // document.body.style.backgroundColor = "white"
+    console.log(randomNumber)
+    console.log(difference)
   } else {
     nextRound()
   }
-console.log(randomNumber)
-console.log(difference)
 }
 function nextRound(){
-  if (round == 1){
-    let randomNumber = Math.floor(Math.random() * 1000) + 5000
-      //import sound from seperate js file
-      // play song
-      warField.play()
-      // record start time of song playing
-      // let songStartTime = Date.now()
-      // declare end time 
-      let endTime
-      console.time('startRoundTimer')
-      // set the timeout function to pause based on random delay
-      setTimeout(function() {
-      warField.pause()
-      endTime = Date.now()
-      let attackButton = document.getElementById('attack')
-      attackButton.disabled = false
-      attackButton.addEventListener('click', function() {
-      let userTime = Date.now()
-      let difference = Math.abs(endTime - userTime)
-      console.log(`Player input time: ${userTime}`)
-      console.log(`Song end time: ${endTime}`)
-      console.log(`Difference: ${difference}`)
-      button.disabled = true
-    })
-    // visual effect i want to implement very unsure of how this will work 
-    // document.body.style.backgroundColor = "black"
-    //add the randomNumber delay function at end
-    }, randomNumber) //generate rand time btw 5-15 secs
-    // document.body.style.backgroundColor = "white"
+  if (round == 0){
+    round++
+    console.log(round)
+  } if (round == 1){
+    round++
+  } if (round == 2){
+    round++
+  } if (round == 3){
+    round++
   } else {
-    nextRound()
+    endGame()
   }
 }
 
