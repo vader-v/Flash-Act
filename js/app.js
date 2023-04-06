4f32e3cae28e6ec435227f23c9b6a049b0c89b65
amirb <amir.bullock9@gmail.com>
2023-04-06 10:52:22 -0400

    add point gain functionality for round winner

--- js/app.js
+++ js/app.js
@@ -10,21 +10,19 @@ import {
 document.addEventListener("DOMContentLoaded", function(){
   
   const songs = [
-    {title: "Snow", audio: snow, round: 0, volume: 0.5},
-    {title: "Warfield", audio: warField, round: 1, volume: 0.4},
-    {title: "Honey and Bleach", audio: hAndB, round: 2, volume: 0.4},
-    {title: "Stress Filled", audio: stressFilled, round: 3, volume: 0.4}
+    {title: "Snow", audio: snow, round: 1, volume: 0.5},
+    {title: "Warfield", audio: warField, round: 2, volume: 0.4},
+    {title: "Honey and Bleach", audio: hAndB, round: 3, volume: 0.4},
+    {title: "Stress Filled", audio: stressFilled, round: 4, volume: 0.4}
   ]
   const numRounds = 5
   const winningScore = 3
   /*---------------------------- Variables (state) ----------------------------*/
   // use of file 
-  let round = 0
+  let round = 1
   let songsIndex = 0
   let player1Score = 0
   let player2Score = 0
-  let player1ScoreDisplay = document.getElementById("player1ScoreDisplay");
-let player2ScoreDisplay = document.getElementById("player2ScoreDisplay");
   let timerIntervalId
   let difference1, difference2
   /*------------------------ Cached Element References ------------------------*/
@@ -38,18 +36,18 @@ let player2ScoreDisplay = document.getElementById("player2ScoreDisplay");
   const attackButton2 = document.getElementById('attack2')
   const resetButton = document.getElementById("reset-button")
   const gameBoard = document.getElementById("gameboard")
+  const winnerElem = document.querySelector("#win-message")
+  const loserElem = document.querySelector("#loss-message")
   /*----------------------------- Event Listeners -----------------------------*/
   
   startButton.addEventListener("click", function(){
     startGame()
+    allowInput(true)
   })
   roundStartButton.addEventListener("click", function(){
     startRound()
   })
-  resetButton.addEventListener("click", function(){
-    resetGame()
-  })
-  
+  resetButton.addEventListener("click", resetGame)
   /*-------------------------------- Functions --------------------------------*/
   function startGame () {
     startRound()
@@ -65,6 +63,8 @@ let player2ScoreDisplay = document.getElementById("player2ScoreDisplay");
   
   function startRound() {
     // playRandom()
+    player1Score = 0
+    player2Score = 0
     startRoundTimerEl.textContent = `Round ${round}`
     startTimer()
   }
@@ -77,7 +77,6 @@ let player2ScoreDisplay = document.getElementById("player2ScoreDisplay");
       if (remainingTime <= 0) {
         clearInterval(timerIntervalId)
         startRoundTimerEl.textContent = ''
-        allowInput(true)
         playRandom()
       }
     }, 1000)
@@ -115,51 +114,59 @@ let player2ScoreDisplay = document.getElementById("player2ScoreDisplay");
     roundStartButton.disabled = false
   }
   function resetGame(){
-    endGame()
-    endRound()
+    //reset variables
+    player1Score = 0
+    player2Score = 0
+    round = 1
+    songsIndex = 0
+    // reset display
+    player1ScoreDisplay.textContent = "0"
+    player2ScoreDisplay.textContent = "0"
+    winnerElem.style.display = "none"
+    loserElem.style.display = "none"
   }
   function endGame(){
     let winner = ""
     let loser = ""
-    if (player1Score === winningScore) { 
-      winner = "Player 1 has won!"
-      loser = "Player 2 has lost"
-    } else if (player2Score === winningScore) {
-      winner = "Player 2 has won!"
-      loser = "Player 1 has lost" 
-    } else {
-      nextRound()
-      return
-    }
-    // update div with win message
-    const winnerElem = document.querySelector("#win-message")
-    winnerElem.textContent = `${winner}`
-    const loserElem = document.querySelector("#loss-message")
-    loserElem.textContent = `${loser}`
-      // hide game board show start screen
-      gameBoard.style.display = "none"
-      startScreen.style.display = "flex"
-    }
-    // let audio
-    
-    function changeSong() {
-      if (songs.audio) {
-        songs.audio.pause();
+    winnerElem.textContent = `${winner} Has won!`
+    loserElem.textContent = `${loser} is the loser.`
+    if (player1Score >= winningScore) { 
+      console.log("Player 1 is the winner!")
+      winner = "Player 1"
+      loser = "Player 2"
+    } else if (player2Score >= winningScore) {
+      console.log("Player 2 is the winner!")
+      winner = "Player 2"
+      loser = "Player 1" 
+        } else {
+          nextRound()
+          return
+        }
+        // update div with win message
+        // hide game board show start screen
+        gameBoard.style.display = "none"
+        startScreen.style.display = "flex"
       }
-      songs.audio = new Audio(`audio/${songsIndex}.mp3`);
-      songs.audio.play();
-      // songs.file.pause()
-      // playSong(round)
+      // let audio
+      
+  function changeSong() {
+    if (songs.audio) {
+    songs.audio.pause();
     }
-    
-    function makeAnimationGo() {
-      header.classList.add('animate__animated', "animate__bounce")
-    }
-    
-    function playRandom() {
-      // change song for the start of the round
-      changeSong(songs[songsIndex].audio)
-      if (round >= 0){
+    songs.audio = new Audio(`audio/${songsIndex}.mp3`);
+    songs.audio.play();
+    // songs.file.pause()
+    // playSong(round)
+  }
+
+  function makeAnimationGo() {
+    header.classList.add('animate__animated', "animate__bounce")
+  }
+
+  function playRandom() {
+    // change song for the start of the round
+    changeSong(songs[songsIndex].audio)
+    if (round > 0){
         // declare random num 
         const randomNumber = Math.floor(Math.random() * 3000) + 6000
         // play song based on what round it is
@@ -170,60 +177,87 @@ let player2ScoreDisplay = document.getElementById("player2ScoreDisplay");
         // set the timeout function to pause based on random delay
         setTimeout(() => {
           stopSong()
-        songEndTime = Date.now()
-        attackButton1.disabled = false
-        attackButton2.disabled = false
+          songEndTime = Date.now()
+          attackButton1.disabled = false
+          attackButton2.disabled = false
+          let player1Clicked = false
+          let player2Clicked = false
         attackButton1.addEventListener('click', () => {
-          let player1Score = document.getElementById("#player1ScoreDisplay")
-          let player2Score = document.getElementById("#player2ScoreDisplay")
-          let player1ScoreValue = document.getElementById("#player1ScoreDisplay")
-          let player2ScoreValue = document.getElementById("#player2ScoreDisplay")
+          player1Clicked = true
+          let player1ScoreDisplay = document.getElementById("player1ScoreDisplay")
+          let player2ScoreDisplay = document.getElementById("player2ScoreDisplay")
+          // let player1Score = parseInt(player1ScoreDisplay.textContent)
+          // let player2Score = parseInt(player2ScoreDisplay.textContent)
+          player1ScoreDisplay.textContent = player1Score.toString()
           const user1Time = Date.now()
           difference1 = Math.abs(songEndTime - user1Time)
-        //this will print a longer number remember to cut off when displaying
-        console.log(`Player 1 input time: ${user1Time}`)
-        console.log(`Song end time: ${songEndTime}`)
-        console.log(`Difference: ${difference1}`)
-        attackButton1.disabled = true
-        attackButton2.disabled = false
-        if (difference1 < difference2){
-          player1Score++
-          console.log('Player 1 wins!')
-          player1Score.textContent =  player1ScoreValue
-          console.log("player 2 score:",player1ScoreValue)
-        } else if (difference2 < difference1) {
-          player2Score++
-          console.log('Player 2 wins!')
-          player2Score.textContent = player2ScoreValue
-          console.log("player 2 score:",player2ScoreValue)
-        }
+          if (player2Clicked){
+            if (difference1 < difference2){
+                player1Score++
+                console.log('Player 1 wins!')
+                player1ScoreDisplay.textContent =  player1Score.toString()
+                console.log("Player 1 score:",player1Score)
+                } else if (difference2 < difference1) {
+                player2Score++
+                console.log('Player 2 wins!')
+                player2ScoreDisplay.textContent = player2Score.toString()
+                console.log("player 2 score:",player2Score)
+                console.log(`Player 1 input time: ${user1Time}`)
+                console.log(`Song end time: ${songEndTime}`)
+                console.log(`Difference: ${difference1}`)
+                attackButton1.disabled = false
+                attackButton2.disabled = false
+                }
+              player1Clicked = false
+              player2Clicked = false
+              } 
+              //this will print a longer number remember to cut off when displaying
       })
       attackButton2.addEventListener('click', () => {
-        let player1ScoreValue = document.getElementById("#player1ScoreDisplay")
-        let player2ScoreValue = document.getElementById("#player2ScoreDisplay")
-        player1ScoreDisplay.textContent = player1Score;
-        player2ScoreDisplay.textContent = player2Score;
-        const user2Time = Date.now()
-        difference2 = Math.abs(songEndTime - user2Time)
-        //this will print a longer number remember to cut off when displaying
-        console.log(`Player 2 input time: ${user2Time}`)
-        console.log(`Song end time: ${songEndTime}`)
-        console.log(`Difference: ${difference2}`)
-        attackButton2.disabled = true
-        attackButton1.disabled = false
-
-        if (difference1 < difference2){
-          player1Score++
-          console.log('Player 1 wins!')
-          player1ScoreDisplay.innerHTML = player1ScoreValue
-          console.log("player 1 score:",`${player1ScoreValue}`)
+        player2Clicked = true
+      let player1ScoreDisplay = document.getElementById("player1ScoreDisplay")
+      let player2ScoreDisplay = document.getElementById("player2ScoreDisplay")
+      player2ScoreDisplay.textContent = player2Score.toString()
+      // let player1Score = parseInt(player1ScoreDisplay.textContent)
+      // let player2Score = parseInt(player2ScoreDisplay.textContent)
+      const user2Time = Date.now()
+      difference2 = Math.abs(songEndTime - user2Time)
+      //this will print a longer number remember to cut off when displaying
+      console.log(`Player 2 input time: ${user2Time}`)
+      console.log(`Song end time: ${songEndTime}`)
+      console.log(`Difference: ${difference2}`)
+      attackButton2.disabled = false
+      attackButton1.disabled = false
+      if (player1Clicked){
+      if (difference1 < difference2){
+        player1Score++
+        console.log('Player 1 wins!')
+        player1ScoreDisplay.textContent = player1Score.toString()
+        console.log("player 1 score:", player1Score)
         } else if (difference2 < difference1) {
-          player2Score++
-          console.log('player 2 wins!')
-          player2ScoreDisplay.innerHTML = player2ScoreValue
-          console.log("player 2 score:", `${player2ScoreValue}`)
+        player2Score++
+        console.log('player 2 wins!')
+        player2ScoreDisplay.textContent = player2Score.toString()
+        console.log("player 2 score:", player2Score)
         } 
+      }
       })
+      let winner = ""
+      let loser = ""
+      winnerElem.textContent = `${winner} Has won!`
+      loserElem.textContent = `${loser} is the loser.`
+      if (player1Score >= winningScore) { 
+        console.log("Player 1 is the winner!")
+        winner = "Player 1"
+        loser = "Player 2"
+      } else if (player2Score >= winningScore) {
+        console.log("Player 2 is the winner!")
+        winner = "Player 2"
+        loser = "Player 1" 
+          } else {
+            nextRound()
+            return
+          }
       songsIndex++
     }, randomNumber) //generate rand time btw 5-15 secs
   }
